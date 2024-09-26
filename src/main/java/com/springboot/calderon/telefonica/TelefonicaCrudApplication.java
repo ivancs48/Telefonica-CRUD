759ee5eb2ed209c5fb6fe609cb62b84b778dbd29
Ivan Calderon <iasuarez@worldoffice.com.co>
2024-09-26 17:56:34 -0500

    primer commit

---
+++ src/main/java/com/springboot/calderon/telefonica/TelefonicaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.calderon.telefonica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelefonicaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelefonicaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/calderon/telefonica/TelefonicaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.calderon.telefonica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelefonicaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
